9279e81ca891b1fec7ee19524bdf4fe863113ca3
Isaac Whitfield <iw@whitfin.io>
2018-10-02 09:26:07 -0700

    Update CI builds to use latest configuration

--- mix.exs
+++ mix.exs
@@ -35,9 +35,9 @@ defmodule Unsafe.Mixfile do
       ],
       preferred_cli_env: [
         docs: :docs,
-        coveralls: :test,
-        "coveralls.html": :test,
-        "coveralls.travis": :test
+        coveralls: :cover,
+        "coveralls.html": :cover,
+        "coveralls.travis": :cover
       ]
     ]
   end
@@ -61,7 +61,7 @@ defmodule Unsafe.Mixfile do
   defp deps do
     [
       { :ex_doc,      "~> 0.16", optional: true, only: [ :docs ] },
-      { :excoveralls, "~> 0.7",  optional: true, only: [ :dev, :test ] }
+      { :excoveralls, "~> 0.7",  optional: true, only: [ :cover ] }
     ]
   end
 end
